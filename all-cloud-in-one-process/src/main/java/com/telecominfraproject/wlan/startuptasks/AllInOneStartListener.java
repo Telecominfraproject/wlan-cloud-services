86b9b8af287299b3eda99d51afd82ac0f0f28bb4
Dmitry Toptygin <dmitry.toptygin@connectus.ai>
2020-04-14 18:00:51 -0400

    addressed some of the warnings in the code

--- all-cloud-in-one-process/src/main/java/com/telecominfraproject/wlan/startuptasks/AllInOneStartListener.java
+++ all-cloud-in-one-process/src/main/java/com/telecominfraproject/wlan/startuptasks/AllInOneStartListener.java
@@ -1,7 +1,5 @@
 package com.telecominfraproject.wlan.startuptasks;
 
-import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
