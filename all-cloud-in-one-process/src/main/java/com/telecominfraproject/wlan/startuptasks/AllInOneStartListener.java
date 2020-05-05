a759b675cf25b89b5514091a00fdc3d87f1a69df
Mike Hansen <mike.hansen@connectus.ai>
2020-05-05 17:30:07 -0400

    Adding Basic Profile and Equipment Creation on Startup for local testing

--- all-cloud-in-one-process/src/main/java/com/telecominfraproject/wlan/startuptasks/AllInOneStartListener.java
+++ all-cloud-in-one-process/src/main/java/com/telecominfraproject/wlan/startuptasks/AllInOneStartListener.java
@@ -1,5 +1,8 @@
 package com.telecominfraproject.wlan.startuptasks;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,100 +11,135 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.telecominfraproject.wlan.core.model.entity.CountryCode;
+import com.telecominfraproject.wlan.core.model.equipment.EquipmentType;
 import com.telecominfraproject.wlan.customer.models.Customer;
 import com.telecominfraproject.wlan.customer.service.CustomerServiceInterface;
+import com.telecominfraproject.wlan.equipment.EquipmentServiceInterface;
+import com.telecominfraproject.wlan.equipment.models.Equipment;
 import com.telecominfraproject.wlan.location.models.Location;
 import com.telecominfraproject.wlan.location.models.LocationDetails;
 import com.telecominfraproject.wlan.location.models.LocationType;
 import com.telecominfraproject.wlan.location.service.LocationServiceInterface;
+import com.telecominfraproject.wlan.profile.ProfileServiceInterface;
+import com.telecominfraproject.wlan.profile.models.Profile;
+import com.telecominfraproject.wlan.profile.models.ProfileType;
 
 /**
- * Listen for context started event so that we can populate initial dataset in the in-memory datastores
+ * Listen for context started event so that we can populate initial dataset in
+ * the in-memory datastores
  */
 @Configuration
 public class AllInOneStartListener implements ApplicationRunner {
-	
-    private static final Logger LOG = LoggerFactory.getLogger(AllInOneStartListener.class);
 
-    @Autowired
-    private CustomerServiceInterface customerServiceInterface;
+	private static final Logger LOG = LoggerFactory.getLogger(AllInOneStartListener.class);
 
-    @Autowired
-    private LocationServiceInterface locationServiceInterface;
+	@Autowired
+	private CustomerServiceInterface customerServiceInterface;
 
-    @Override
-    public void run(ApplicationArguments args) {
-    	        LOG.info("Creating initial objects");
+	@Autowired
+	private LocationServiceInterface locationServiceInterface;
 
-    	        Customer customer = new Customer();
-    	        customer.setEmail("test@example.com");
-    	        customer.setName("Test Customer");
-    	        
-    			customer = customerServiceInterface.create(customer );
+	@Autowired
+	private EquipmentServiceInterface equipmentServiceInterface;
 
-		       Location location_1 = new Location();
-		       location_1.setParentId(0);
-		       location_1.setCustomerId(customer.getId());
-		       location_1.setLocationType(LocationType.SITE);
-		       location_1.setName("Menlo Park");
-		       location_1.setDetails(LocationDetails.createWithDefaults());
-		       location_1.getDetails().setCountryCode(CountryCode.usa);
-		       
-		       location_1 = locationServiceInterface.create(location_1);
-		       
-		       Location location_1_1 = new Location();
-		       location_1_1.setParentId(location_1.getId());
-		       location_1_1.setCustomerId(customer.getId());
-		       location_1_1.setLocationType(LocationType.BUILDING);
-		       location_1_1.setName("Building 1");
-		       
-		       location_1_1 = locationServiceInterface.create(location_1_1);
+	@Autowired
+	private ProfileServiceInterface profileServiceInterface;
 
-		       Location location_1_1_1 = new Location();
-		       location_1_1_1.setParentId(location_1_1.getId());
-		       location_1_1_1.setCustomerId(customer.getId());
-		       location_1_1_1.setLocationType(LocationType.FLOOR);
-		       location_1_1_1.setName("Floor 1");
-		       
-		       location_1_1_1 = locationServiceInterface.create(location_1_1_1);
+	@Override
+	public void run(ApplicationArguments args) {
+		LOG.info("Creating initial objects");
 
-		       Location location_1_1_2 = new Location();
-		       location_1_1_2.setParentId(location_1_1.getId());
-		       location_1_1_2.setCustomerId(customer.getId());
-		       location_1_1_2.setLocationType(LocationType.FLOOR);
-		       location_1_1_2.setName("Floor 2");
-		       
-		       location_1_1_2 = locationServiceInterface.create(location_1_1_2);
+		Customer customer = new Customer();
+		customer.setEmail("test@example.com");
+		customer.setName("Test Customer");
 
-		       Location location_1_1_3 = new Location();
-		       location_1_1_3.setParentId(location_1_1.getId());
-		       location_1_1_3.setCustomerId(customer.getId());
-		       location_1_1_3.setLocationType(LocationType.FLOOR);
-		       location_1_1_3.setName("Floor 3");
-		       
-		       location_1_1_3 = locationServiceInterface.create(location_1_1_3);
+		customer = customerServiceInterface.create(customer);
 
-		       Location location_1_2 = new Location();
-		       location_1_2.setParentId(location_1.getId());
-		       location_1_2.setCustomerId(customer.getId());
-		       location_1_2.setLocationType(LocationType.BUILDING);
-		       location_1_2.setName("Building 2");
-		       
-		       location_1_2 = locationServiceInterface.create(location_1_2);
+		Location location_1 = new Location();
+		location_1.setParentId(0);
+		location_1.setCustomerId(customer.getId());
+		location_1.setLocationType(LocationType.SITE);
+		location_1.setName("Menlo Park");
+		location_1.setDetails(LocationDetails.createWithDefaults());
+		location_1.getDetails().setCountryCode(CountryCode.usa);
 
-		       
-		       Location location_2 = new Location();
-		       location_2.setParentId(0);
-		       location_2.setCustomerId(customer.getId());
-		       location_2.setLocationType(LocationType.SITE);
-		       location_2.setName("Ottawa");
-		       location_2.setDetails(LocationDetails.createWithDefaults());
-		       location_2.getDetails().setCountryCode(CountryCode.ca);
+		location_1 = locationServiceInterface.create(location_1);
 
-		       location_2 = locationServiceInterface.create(location_2);
+		Location location_1_1 = new Location();
+		location_1_1.setParentId(location_1.getId());
+		location_1_1.setCustomerId(customer.getId());
+		location_1_1.setLocationType(LocationType.BUILDING);
+		location_1_1.setName("Building 1");
 
-    			LOG.info("Done creating initial objects");
-    	        
-    }
-    
+		location_1_1 = locationServiceInterface.create(location_1_1);
+
+		Location location_1_1_1 = new Location();
+		location_1_1_1.setParentId(location_1_1.getId());
+		location_1_1_1.setCustomerId(customer.getId());
+		location_1_1_1.setLocationType(LocationType.FLOOR);
+		location_1_1_1.setName("Floor 1");
+
+		location_1_1_1 = locationServiceInterface.create(location_1_1_1);
+
+		Location location_1_1_2 = new Location();
+		location_1_1_2.setParentId(location_1_1.getId());
+		location_1_1_2.setCustomerId(customer.getId());
+		location_1_1_2.setLocationType(LocationType.FLOOR);
+		location_1_1_2.setName("Floor 2");
+
+		location_1_1_2 = locationServiceInterface.create(location_1_1_2);
+
+		Location location_1_1_3 = new Location();
+		location_1_1_3.setParentId(location_1_1.getId());
+		location_1_1_3.setCustomerId(customer.getId());
+		location_1_1_3.setLocationType(LocationType.FLOOR);
+		location_1_1_3.setName("Floor 3");
+
+		location_1_1_3 = locationServiceInterface.create(location_1_1_3);
+
+		Location location_1_2 = new Location();
+		location_1_2.setParentId(location_1.getId());
+		location_1_2.setCustomerId(customer.getId());
+		location_1_2.setLocationType(LocationType.BUILDING);
+		location_1_2.setName("Building 2");
+
+		location_1_2 = locationServiceInterface.create(location_1_2);
+
+		Location location_2 = new Location();
+		location_2.setParentId(0);
+		location_2.setCustomerId(customer.getId());
+		location_2.setLocationType(LocationType.SITE);
+		location_2.setName("Ottawa");
+		location_2.setDetails(LocationDetails.createWithDefaults());
+		location_2.getDetails().setCountryCode(CountryCode.ca);
+
+		location_2 = locationServiceInterface.create(location_2);
+
+		Profile profileAp = new Profile();
+		profileAp.setCustomerId(customer.getId());
+		profileAp.setName("ApProfile");
+		profileAp.setProfileType(ProfileType.equipment_ap);
+		profileServiceInterface.create(profileAp);
+
+		Profile profileSsid = new Profile();
+		profileSsid.setCustomerId(customer.getId());
+		profileSsid.setProfileType(ProfileType.ssid);
+		profileSsid.setName("Connectus-cloud");
+		Set<Long> childProfileIds = new HashSet<Long>();
+		childProfileIds.add(profileAp.getId());
+		profileSsid.setChildProfileIds(childProfileIds);
+		profileServiceInterface.create(profileSsid);
+
+		Equipment equipment = new Equipment();
+		equipment.setCustomerId(customer.getId());
+		equipment.setEquipmentType(EquipmentType.AP);
+		equipment.setLocationId(location_2.getId());
+		equipment.setProfileId(profileAp.getId());
+
+		equipment = equipmentServiceInterface.create(equipment);
+
+		LOG.info("Done creating initial objects");
+
+	}
+
 }
