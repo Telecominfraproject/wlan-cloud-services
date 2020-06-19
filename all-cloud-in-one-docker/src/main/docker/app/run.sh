1cf5d24a664302b43a610e7213d2df5975758d91
Lynn Shi <lynnshi@MacBook-Pro.local>
2020-06-19 10:58:26 -0400

    openSyncGW cloud docker NBI and SBI support

--- all-cloud-in-one-docker/src/main/docker/app/run.sh
+++ all-cloud-in-one-docker/src/main/docker/app/run.sh
@@ -2,6 +2,18 @@
 
 LOGGING_PROPS=" -Dlogging.config=file:/app/intcloudcomp/logback.xml"
 
-export ALL_PROPS="$LOGGING_PROPS"
+BACKEND_SERVER="${BACKEND_SERVER}"
+
+HOST_PROPS=" "
+if [[ -n $BACKEND_SERVER ]]
+then
+  echo Use specifed local host
+  HOST_PROPS+=" -Dtip.wlan.externalHostName=$BACKEND_SERVER"
+  HOST_PROPS+=" -Dtip.wlan.internalHostName=$BACKEND_SERVER"
+  HOST_PROPS+=" -Dtip.wlan.introspectTokenApi.host=${BACKEND_SERVER}:9091"
+fi
+#echo HOST_PROPS $HOST_PROPS
+
+export ALL_PROPS="$LOGGING_PROPS $HOST_PROPS"
 
 java $ALL_PROPS -jar app.jar
