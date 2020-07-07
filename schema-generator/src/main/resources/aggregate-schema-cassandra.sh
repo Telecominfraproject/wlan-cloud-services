70ce8b38d4414d7c429fb0aa4e7d257c0aec5125
Rahul Sharma <rahul.sharma@connectus.ai>
2020-07-07 18:47:48 -0400

    TW-825: update SSC to use Cassandra

--- schema-generator/src/main/resources/aggregate-schema-cassandra.sh
+++ schema-generator/src/main/resources/aggregate-schema-cassandra.sh
@@ -14,7 +14,7 @@ cd $SCHEMA_DIR
 generateSchemaFile() {
   cat $SCHEMA_DIR/src/main/resources/user_create_cassandra.cql >> $1
   find ../. -type f -name "schema-cassandra.cql" | grep -v target |xargs cat >> $1
-  echo "\q " >> $1
+  echo "DESCRIBE TABLES; " >> $1
   mv $1 $SCHEMA_DIR/target
 }
 
@@ -22,7 +22,7 @@ generateSchemaFile() {
 # $1: Aggregated SQLFile
 # $2: Artifactory Username
 # $3: Encrypted Artifactory Password
-# $4: Artifactory Target file Path. E.g.; "/0.0.1-SNAPSHOT/sql/cloud-sdk-schema-cassandra.sql"
+# $4: Artifactory Target file Path. E.g.; "/0.0.1-SNAPSHOT/cql/cloud-sdk-schema-cassandra.cql"
 deployToArtifactory() {
   curl -u$2:$3 -T $SCHEMA_DIR/target/$1 "$4/$5"
 }
--- ssc-docker/src/main/docker/app/run.sh
+++ ssc-docker/src/main/docker/app/run.sh
@@ -2,6 +2,15 @@
 
 LOGGING_PROPS=" -Dlogging.config=file:/app/ssc/logback.xml"
 
-export ALL_PROPS="$LOGGING_PROPS"
+CASSANDRA_HOST="tip-wlan-cassandra-headless:9042"
+CASSANDRA_TIP_USER="tip_user"
+CASSANDRA_TIP_PASSWORD="tip_password"
+
+DATABASE_PROPS=" "
+DATABASE_PROPS+=" -Ddatastax-java-driver.basic.contact-points.0=$CASSANDRA_HOST"
+DATABASE_PROPS+=" -Ddatastax-java-driver.advanced.auth-provider.username=$CASSANDRA_USER"
+DATABASE_PROPS+=" -Ddatastax-java-driver.advanced.auth-provider.password=$CASSANDRA_PASSWORD"
+
+export ALL_PROPS="$LOGGING_PROPS $DATABASE_PROPS"
 
 java $ALL_PROPS -jar app.jar
