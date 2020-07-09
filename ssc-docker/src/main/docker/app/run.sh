a753480547f6789e43b24dd1bb606cb2cd3b68bc
Rahul Sharma <rahul.sharma@connectus.ai>
2020-07-09 14:42:16 -0400

    TW-837: Removing the separate datastax override variables and instead override cassandra-application.conf to update these parameters all at the same time.

--- ssc-docker/src/main/docker/app/run.sh
+++ ssc-docker/src/main/docker/app/run.sh
@@ -2,14 +2,7 @@
 
 LOGGING_PROPS=" -Dlogging.config=file:/app/ssc/logback.xml"
 
-CASSANDRA_HOST="tip-wlan-cassandra-headless:9042"
-CASSANDRA_TIP_USER="tip_user"
-CASSANDRA_TIP_PASSWORD="tip_password"
-
-DATABASE_PROPS=" "
-DATABASE_PROPS+=" -Ddatastax-java-driver.basic.contact-points.0=$CASSANDRA_HOST"
-DATABASE_PROPS+=" -Ddatastax-java-driver.advanced.auth-provider.username=$CASSANDRA_TIP_USER"
-DATABASE_PROPS+=" -Ddatastax-java-driver.advanced.auth-provider.password=$CASSANDRA_TIP_PASSWORD"
+DATABASE_PROPS=" -Dtip.wlan.cassandraConfigFileName=/app/ssc/cassandra-application.conf"
 
 export ALL_PROPS="$LOGGING_PROPS $DATABASE_PROPS"
 
