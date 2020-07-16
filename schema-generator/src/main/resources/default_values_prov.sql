6e67a94d02b0a0e2fe397bab0523004691c4c735
Rahul Sharma <rahul.sharma@connectus.ai>
2020-07-16 17:31:09 -0400

    TW-883: Update schema generator to produce 2 SQL scripts for Prov server TW-884: Update generated SQL script with default Customer and location data

---
+++ schema-generator/src/main/resources/default_values_prov.sql
@@ -0,0 +1,9 @@
+insert into customer_info (id, email, name, createdTimestamp, lastModifiedTimestamp)
+values (2, 'tip-customer@tip.org', 'TIP Customer Inc.', 0,0) ON CONFLICT (id) DO NOTHING;
+
+insert into equipment_location (id, locationType, customerId, name, parentId, createdTimestamp, lastModifiedTimestamp)
+values (1, 1, 2, 'Ottawa', null, 0,0) ON CONFLICT (id) DO NOTHING;
+insert into equipment_location (id, locationType, customerId, name, parentId, createdTimestamp, lastModifiedTimestamp)
+values (2, 2, 2, 'TipBuilding', 1, 0,0) ON CONFLICT (id) DO NOTHING;
+insert into equipment_location (id, locationType, customerId, name, parentId, createdTimestamp, lastModifiedTimestamp)
+values (3, 3, 2, 'FirstFloor', 2, 0,0) ON CONFLICT (id) DO NOTHING;
