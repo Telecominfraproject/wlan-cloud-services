35047c4fe73b83fdaf305bc6c6ec7f2c19a70935
Kareem Dabbour <kareemdabbour@Kareems-MacBook-Pro.local>
2021-05-19 18:39:52 -0400

    WIFI-2398 Adding ClientMac to client session system events

--- client-models/src/main/java/com/telecominfraproject/wlan/client/models/events/ClientAddedEvent.java
+++ client-models/src/main/java/com/telecominfraproject/wlan/client/models/events/ClientAddedEvent.java
@@ -1,17 +1,28 @@
 package com.telecominfraproject.wlan.client.models.events;
 
 import com.telecominfraproject.wlan.client.models.Client;
+import com.telecominfraproject.wlan.core.model.equipment.MacAddress;
+import com.telecominfraproject.wlan.core.model.json.interfaces.HasClientMac;
 import com.telecominfraproject.wlan.systemevent.models.CustomerEventWithPayload;
 
 /**
  * @author dtoptygin
  *
  */
-public class ClientAddedEvent extends CustomerEventWithPayload<Client> {
+public class ClientAddedEvent extends CustomerEventWithPayload<Client> implements HasClientMac{
     private static final long serialVersionUID = 7142208487917559985L;
 
     public ClientAddedEvent(Client client){
         super(client.getCustomerId(), client);
+    }
+    
+    @Override
+    public MacAddress getClientMacAddress() {
+    	if (getPayload() != null) {
+    		return getPayload().getMacAddress();
+    	}
+    	
+    	return null; 
     }
     
     /**
--- client-models/src/main/java/com/telecominfraproject/wlan/client/models/events/ClientChangedEvent.java
+++ client-models/src/main/java/com/telecominfraproject/wlan/client/models/events/ClientChangedEvent.java
@@ -1,17 +1,28 @@
 package com.telecominfraproject.wlan.client.models.events;
 
 import com.telecominfraproject.wlan.client.models.Client;
+import com.telecominfraproject.wlan.core.model.equipment.MacAddress;
+import com.telecominfraproject.wlan.core.model.json.interfaces.HasClientMac;
 import com.telecominfraproject.wlan.systemevent.models.CustomerEventWithPayload;
 
 /**
  * @author dtoptygin
  *
  */
-public class ClientChangedEvent extends CustomerEventWithPayload<Client> {
+public class ClientChangedEvent extends CustomerEventWithPayload<Client> implements HasClientMac{
     private static final long serialVersionUID = 7142209997917559985L;
 
     public ClientChangedEvent(Client client){
         super(client.getCustomerId(), client);
+    }
+    
+    @Override
+    public MacAddress getClientMacAddress() {
+    	if (getPayload() != null) {
+    		return getPayload().getMacAddress();
+    	}
+    	
+    	return null; 
     }
     
     /**
