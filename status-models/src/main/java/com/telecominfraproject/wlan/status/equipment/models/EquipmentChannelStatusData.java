04a13877d19d15603fc14d5c598d02f246c07535
Thomas-Leung2021 <thomas.leung@netexperience.com>
2021-06-25 12:39:30 -0400

    add tx_power status

--- status-models/src/main/java/com/telecominfraproject/wlan/status/equipment/models/EquipmentChannelStatusData.java
+++ status-models/src/main/java/com/telecominfraproject/wlan/status/equipment/models/EquipmentChannelStatusData.java
@@ -12,6 +12,7 @@ public class EquipmentChannelStatusData extends StatusDetails {
     private static final long serialVersionUID = 470569467119609438L;
 
     private Map<RadioType, Integer> channelNumberStatusDataMap = new EnumMap<>(RadioType.class);
+    private Map<RadioType, Integer> txPowerDataMap = new EnumMap<>(RadioType.class);
 
 
     public EquipmentChannelStatusData()
@@ -34,9 +35,17 @@ public class EquipmentChannelStatusData extends StatusDetails {
     public Map<RadioType, Integer> getChannelNumberStatusDataMap() {
         return channelNumberStatusDataMap;
     }
+    
+    public Map<RadioType, Integer> getTxPowerDataMap() {
+        return txPowerDataMap;
+    }
 
     public void setChannelNumberStatusDataMap(Map<RadioType, Integer> channelNumberStatusDataMap) {
         this.channelNumberStatusDataMap = channelNumberStatusDataMap;
+    }
+    
+    public void setTxPowerDataMap(Map<RadioType, Integer> txPowerDataMap) {
+        this.txPowerDataMap = txPowerDataMap;
     }
 
     @Override
@@ -49,19 +58,33 @@ public class EquipmentChannelStatusData extends StatusDetails {
             	result.channelNumberStatusDataMap.put(k, v);
             });
         }
+        
+        if (getTxPowerDataMap() != null) {
+            result.setTxPowerDataMap(new EnumMap<>(RadioType.class));
+            
+            this.txPowerDataMap.forEach((k, v) -> {
+                result.txPowerDataMap.put(k, v);
+            });
+        }
         return result;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        EquipmentChannelStatusData other = (EquipmentChannelStatusData) obj;
-        return Objects.equals(channelNumberStatusDataMap, other.channelNumberStatusDataMap);
+    public int hashCode() {
+        return Objects.hash(channelNumberStatusDataMap, txPowerDataMap);
     }
 
     @Override
-    public int hashCode() {
-    	return Objects.hash(channelNumberStatusDataMap);
-    	
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EquipmentChannelStatusData other = (EquipmentChannelStatusData) obj;
+        return Objects.equals(channelNumberStatusDataMap, other.channelNumberStatusDataMap)
+                && Objects.equals(txPowerDataMap, other.txPowerDataMap);
     }
 
 }
