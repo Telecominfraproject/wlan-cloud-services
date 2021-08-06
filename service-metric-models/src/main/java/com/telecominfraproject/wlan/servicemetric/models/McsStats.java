d0e68663452df6b0880788280efb8a30f66a5048
Thomas-Leung2021 <thomas.leung@netexperience.com>
2021-08-06 16:38:37 -0400

    [WIFI-3358] remove unused attributes in McsStats

--- service-metric-models/src/main/java/com/telecominfraproject/wlan/servicemetric/models/McsStats.java
+++ service-metric-models/src/main/java/com/telecominfraproject/wlan/servicemetric/models/McsStats.java
@@ -13,16 +13,6 @@ public class McsStats extends BaseJsonModel
     *  The MCS number. This is table index.
     */
     private McsType mcsNum;
-
-    /**
-    *  The number of successfully transmitted frames at this rate. Do not count failed transmission.
-    */
-    private Integer txFrames;
-
-    /**
-    * The number of received frames at this rate.
-    */
-    private Integer rxFrames;
     
     private Long rate;
     
@@ -32,46 +22,23 @@ public class McsStats extends BaseJsonModel
         // for serialization
     }
 
-
     public McsType getMcsNum() {
         return mcsNum;
     }
 
-
     public void setMcsNum(McsType mcsNum) {
         this.mcsNum = mcsNum;
-    }
-
-
-    public Integer getTxFrames() {
-        return txFrames;
-    }
-
-
-    public void setTxFrames(Integer txFrames) {
-        this.txFrames = txFrames;
-    }
-
-
-    public Integer getRxFrames() {
-        return rxFrames;
-    }
-
-
-    public void setRxFrames(Integer rxFrames) {
-        this.rxFrames = rxFrames;
     }
    
     public Long getRate() {
         return rate;
     }
 
-
     public void setRate(Long rate) {
         this.rate = rate;
     }
 
-
+    
     @Override
     public McsStats clone() {
         McsStats ret = (McsStats) super.clone();
